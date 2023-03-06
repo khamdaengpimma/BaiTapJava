5bd3a29b02b1aae4e07e0efa6d25e2514200c0a5
khamdaeng phimmasone <118417111+khamdaengpimma@users.noreply.github.com>
2023-03-06 08:17:46 +0700

    add image PizzaClient

--- src/_Pizza/PizzaClient.java
+++ src/_Pizza/PizzaClient.java
@@ -2,12 +2,12 @@ package _Pizza;
 
 public class PizzaClient {
     public void run(){
-        Pizza round = new Pizza (12.5, new Circle (3.5));
-        Pizza rect =  new Pizza (19.9, new Rectangle (5, 4));
+        Pizza p1 = new Pizza (12.5, new Circle (3.5));
+        Pizza p2 =  new Pizza (19.9, new Rectangle (5, 4));
         PizzaDeal pid = new PizzaDeal();
 
-        System.out.println(round + " is a better deal than " + rect
-                + ": " + pid.betterDeal(round, rect));
+        System.out.println(p1 + " is a better deal than " + p2
+                + ": " + pid.betterDeal(p1, p2));
     }
     public static void main(String[] args){
         new PizzaClient().run();
